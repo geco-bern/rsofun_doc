a94459466942b19d8b98a974495dbff266dfe467
Beni Stocker <benjamin.stocker@gmail.com>
2025-06-06 17:04:48 +0200

    added files and update

--- analysis/03_bayesian_calibration.R
+++ analysis/03_bayesian_calibration.R
@@ -9,14 +9,8 @@ library(sensitivity)
 library(BayesianTools)
 library(tictoc)
 
-# Read data produced with 01_sample_sites.R
-drivers <- read_rds(here::here("data/drivers_train.rds"))
+## Function definitions ----------------------------------------------------------
 
-validation <- drivers_train |>
-  select(sitename, data = forcing) |>
-  mutate(data = purrr::map(data, ~select(., date, gpp)))
-
-# Define functions for plotting
 getSetup <- function(x) {
   classes <- class(x)
   if (any(c('mcmcSampler', 'smcSampler') %in% classes)) x$setup
@@ -42,9 +36,9 @@ t_col <- function(color, percent = 50, name = NULL) {
   invisible(t.col)
 }
 
-plot_prior_posterior_density <- function(
-    x               # Bayesian calibration output
-){
+# Bayesian calibration output
+plot_prior_posterior_density <- function(x){
+
   # Get matrices of prior and posterior samples
   posteriorMat <- getSample(x, parametersOnly = TRUE)
   priorMat <-  getSetup(x)$prior$sampler(10000) # nPriorDraws = 10000
@@ -55,9 +49,10 @@ plot_prior_posterior_density <- function(
   colnames(priorMat) <- parNames
 
   # Create data frame for plotting
-  df_plot <- rbind(data.frame(posteriorMat, distrib = "posterior"),
-                   data.frame(priorMat, distrib = "prior")
-  )
+  df_plot <- rbind(
+    data.frame(posteriorMat, distrib = "posterior"),
+    data.frame(priorMat, distrib = "prior")
+    )
   df_plot$distrib <- as.factor(df_plot$distrib)
 
   # Plot with facet wrap
@@ -69,87 +64,32 @@ plot_prior_posterior_density <- function(
     geom_density() +
     theme_classic() +
     facet_wrap( ~ variable , nrow = 2, scales = "free") +
-    theme(legend.position = "bottom",
-          axis.title.x = element_text("")) +
+    theme(
+      legend.position = "bottom",
+      axis.title.x = element_text("")
+      ) +
     scale_fill_manual(NULL, values = c("#29a274ff", t_col("#777055ff"))) # GECO colors
 
   return(gg)
 }
 
-# Set random seed for reproducibility
-set.seed(432)
-
-# Define calibration settings
-settings_calib <- list(
-  method = "BayesianTools",
-  metric = rsofun::cost_likelihood_pmodel,
-  control = list(
-    sampler = "DEzs",
-    settings = list(
-      burnin = 10000,
-      iterations = 50000,
-      nrChains = 3,       # number of independent chains
-      startValue = 3      # number of internal chains to be sampled
-    )),
-  par = list(
-    kphio = list(lower = 0.02, upper = 0.15, init = 0.05),
-    kphio_par_a =list(lower = -0.004, upper = -0.001, init = -0.0025),
-    kphio_par_b = list(lower = 10, upper = 30, init = 20),
-    soilm_thetastar = list(
-      lower = 0.01 * rsofun::p_model_drivers$site_info[[1]]$whc,
-      upper = 1.0  * rsofun::p_model_drivers$site_info[[1]]$whc,
-      init  = 0.6  * rsofun::p_model_drivers$site_info[[1]]$whc
-    ),
-    soilm_betao = list(lower = 0.0, upper = 1.0, init = 0.0),
-    err_gpp = list(lower = 0.1, upper = 3, init = 0.8)
-  )
-)
-
-# Run calibration and measure time
-tic()
-
-par_calib <- calib_sofun(
-  drivers = drivers,
-  obs = validation,
-  settings = settings_calib,
-  par_fixed = list(
-    beta_unitcostratio = 146.0,
-    kc_jmax            = 0.41,
-    rd_to_vcmax        = 0.014,
-    tau_acclim         = 20.0
-    ),
-  targets = "gpp"
-)
-
-toc() # Stop measuring time
-
-# Plot prior and posterior distributions
-gg <- plot_prior_posterior_density(par_calib$mod)
-gg
-
-get_runtime <- function(par_calib) {# function(settings_calib){
+get_runtime <- function(out_calib) {# function(settings_calib){
   total_time_secs <- sum(unlist(lapply(
-    par_calib$mod,
+    out_calib$mod,
     function(curr_chain){curr_chain$settings$runtime[["elapsed"]]})))
   return(sprintf("Total runtime: %.0f secs", total_time_secs))
 }
 
-# Plot MCMC diagnostics
-plot(par_calib$mod)
-summary(par_calib$mod) # Gives Gelman Rubin multivariate of 1.019
-summary(par_calib$par)
-print(get_runtime(par_calib))
+get_settings_str <- function(out_calib) {
 
-# Output
-## Define MCMC postprocessing
-get_settings_str <- function(par_calib) {# function(settings_calib){
-  stopifnot(is(par_calib$mod, "mcmcSamplerList"))
+  stopifnot(is(out_calib$mod, "mcmcSamplerList"))
 
   # explore what's in a mcmcSamplerList:
-  # summary(par_calib$mod)
-  # plot(par_calib$mod)
-  individual_chains <- par_calib$mod
+  # summary(out_calib$mod)
+  # plot(out_calib$mod)
+  individual_chains <- out_calib$mod
   nrChains <- length(individual_chains) # number of chains
+
   # plot(individual_chains[[1]]) # chain 1
   # plot(individual_chains[[2]]) # chain 2
   # plot(individual_chains[[3]]) # chain 3
@@ -157,20 +97,129 @@ get_settings_str <- function(par_calib) {# function(settings_calib){
   # individual_chains[[1]]$chain
   # individual_chains[[1]]$X
   # individual_chains[[1]]$Z
-  nrInternalChains <- lapply(individual_chains, function(curr_chain){curr_chain$settings$nrChains})  |> unlist() |> unique() |> paste0(collapse = "-")
-  nrIterations     <- lapply(individual_chains, function(curr_chain){curr_chain$settings$iterations})|> unlist() |> unique() |> paste0(collapse = "-")
-  nrBurnin         <- lapply(individual_chains, function(curr_chain){curr_chain$settings$burnin})    |> unlist() |> unique() |> paste0(collapse = "-")
-  sampler_name     <- lapply(individual_chains, function(curr_chain){curr_chain$settings$sampler})   |> unlist() |> unique() |> paste0(collapse = "-")
+
+  nrInternalChains <- lapply(
+    individual_chains, 
+    function(curr_chain){curr_chain$settings$nrChains})  |> 
+      unlist() |> 
+      unique() |> 
+      paste0(collapse = "-")
+
+  nrIterations <- lapply(
+    individual_chains, 
+    function(curr_chain){curr_chain$settings$iterations})|> 
+      unlist() |> 
+      unique() |> 
+      paste0(collapse = "-")
+
+  nrBurnin <- lapply(
+    individual_chains, 
+    function(curr_chain){curr_chain$settings$burnin})    |> 
+      unlist() |> 
+      unique() |> 
+      paste0(collapse = "-")
+
+  sampler_name <- lapply(
+    individual_chains, 
+    function(curr_chain){curr_chain$settings$sampler})   |> 
+      unlist() |> 
+      unique() |> 
+      paste0(collapse = "-")
 
   # create descriptive string of settings for filename
-  return(sprintf(
-    "Sampler-%s-%siterations_ofwhich%sburnin_chains_%sx%s_",
-    sampler_name, nrIterations, nrBurnin, nrChains,  nrInternalChains))
+  return(
+    sprintf(
+      "Case-%s-Sampler-%s-%siterations_ofwhich%sburnin_chains_%sx%s_",
+      out_calib$name,
+      sampler_name, 
+      nrIterations, 
+      nrBurnin, 
+      nrChains,  
+      nrInternalChains
+      )
+    )
 }
 
-settings_string <- get_settings_str(par_calib)
+# Function to run calibration and write output for an individual calibration setup (e.g., by site)
+calib_sofun_bycase <- function(drivers_bycase, case_name = "global"){
 
-saveRDS(par_calib, file = here::here("data/par_calib.rds"))
+  validation_bycase <- drivers_bycase |>
+    select(sitename, data = forcing) |>
+    mutate(data = purrr::map(data, ~select(., date, gpp)))
+
+  # Common calibration settings for all cases
+  settings_calib <- list(
+    method = "BayesianTools",
+    metric = rsofun::cost_likelihood_pmodel,
+    control = list(
+      sampler = "DEzs",
+      settings = list(
+        burnin = 10000,
+        iterations = 50000,
+        nrChains = 3,       # number of independent chains
+        startValue = 3      # number of internal chains to be sampled
+      )),
+    par = list(
+      kphio = list(lower = 0.02, upper = 0.15, init = 0.05),
+      kphio_par_a =list(lower = -0.004, upper = -0.001, init = -0.0025),
+      kphio_par_b = list(lower = 10, upper = 30, init = 20),
+      soilm_thetastar = list(
+        lower = 0.01 * drivers_bycase$site_info[[1]]$whc,
+        upper = 1.0  * drivers_bycase$site_info[[1]]$whc,
+        init  = 0.6  * drivers_bycase$site_info[[1]]$whc
+      ),
+      soilm_betao = list(lower = 0.0, upper = 1.0, init = 0.0),
+      err_gpp = list(lower = 0.1, upper = 3, init = 0.8)
+    )
+  )
+
+  out <- calib_sofun(
+    drivers = drivers_bycase,
+    obs = validation_bycase,
+    settings = settings_calib,
+    par_fixed = list(
+      beta_unitcostratio = 146.0,
+      kc_jmax            = 0.41,
+      rd_to_vcmax        = 0.014,
+      tau_acclim         = 20.0
+      ),
+    targets = "gpp"
+  )
+
+  out$name <- case_name
+
+  settings_string <- get_settings_str(out)
+
+  filnam <- here::here(paste0("data/out_calib_", settings_string, ".rds"))
+
+  saveRDS(out, file = filnam)
+
+  return(filnam)
+
+}
+
+## Read data ---------------------------------------------------------------------
+# Read data produced with 01_sample_sites.R
+drivers <- read_rds(here::here("data/drivers_train.rds"))
+
+## Run calibrations ------------------------------------------------------------------
+### Global calibration (all sites) ---------------------------------------------------
+# Set random seed for reproducibility
+set.seed(432)
+
+# Run calibration
+filnam_global <- calib_sofun_bycase(drivers, case_name = "global")
+
+out_calib_global <- readRDS(filnam_global)
+
+# Plot MCMC diagnostics
+plot(out_calib_global$mod)
+summary(out_calib_global$mod) # Gives Gelman Rubin multivariate of 1.019
+summary(out_calib_global$par)
+print(get_runtime(out_calib_global))
+
+# Plot prior and posterior distributions
+gg <- plot_prior_posterior_density(out_calib_global$mod)
 
 ggsave(here::here("fig/prior_posterior.pdf"), plot = gg, width = 6, height = 5)
 ggsave(here::here("fig/prior_posterior.png"), plot = gg, width = 6, height = 5)
