8745e38662852835de79a08a86213aa4ea5380b2
Beni Stocker <benjamin.stocker@gmail.com>
2025-07-01 14:53:17 +0200

    added demo with Nick Smith's data

--- data-raw/vj_data.R
+++ data-raw/vj_data.R
@@ -1,23 +1,113 @@
 library(tidyverse)
+library(rpmodel)
 
-df <- read_csv("~/data/archive/acitglob_kumarathunge_2020/data/Kumarathunge-aci-tglob-8595d961d4c8/Data/PPC-TGlob_V1.0.csv")
-
-# filter data to field - native environment (seed source lon/lat can be interpreted as lon/lat for forcing data)
-df <- df |>
-  filter(Growth_condition == "Field (NE)")
+# Nick Smith's data,
+df <- read_csv(here::here("data/GlobV_v2.1_env_open.csv"))
 
 df_sites <- df |>
   ungroup() |>
-  mutate(vj = Vcmax/Jmax) |>
-  group_by(Location) |>
+  mutate(vj = vcmax/jmax) |>
+  mutate(site = paste0("lon_", as.character(format(longitude, digits = 2)), "_lat_", as.character(format(latitude, digits = 3)))) |>
+  group_by(site) |>
   summarise(
-    lon = mean(seed_source_longitude),
-    lat = mean(seed_source_latitude),
-    vcmax = mean(Vcmax),
-    jmax = mean(Jmax),
+    temp = mean(temperature_gs),
+    vpd = mean(vpd_gs),
+    par = mean(par_gs),
+    elv = mean(z),
+    vcmax = mean(vcmax),
+    jmax = mean(jmax),
     vj = mean(vj)
-    )
+  ) |>
+  mutate(
+    patm = rpmodel::calc_patm(elv)
+  )
 
 df_sites |>
   ggplot(aes(x = vj)) +
-  geom_histogram(bins = 9)
+  geom_histogram(bins = 15)
+
+# Apply one-step P-model function on each row of df_sites
+library(dplyr)
+library(purrr)
+library(rsofun)  # install from branch simple_pmodel
+
+# Define constant model parameters
+params_modl <- list(
+  kphio              = 0.04998,
+  kphio_par_a        = 0.0,
+  kphio_par_b        = 1.0,
+  beta_unitcostratio = 146.0,
+  rd_to_vcmax        = 0.014,
+  kc_jmax            = 0.41
+)
+
+# Apply the model function row-wise and bind results
+df_sites_with_outputs <- df_sites |>
+  mutate(row_id = row_number()) |>
+  pmap_dfr(function(temp, vpd, patm, par, row_id, ...) {
+    result <- run_pmodel_onestep_f_bysite(
+      lc4 = FALSE,
+      forcing = data.frame(temp = temp, vpd = vpd, patm = patm, ppfd = par, co2 = 400),
+      params_modl = params_modl,
+      makecheck = FALSE
+    ) |>
+      rename(vcmax_mod = vcmax, jmax_mod = jmax)
+    cbind(row_id = row_id, result)
+  }) |>
+  right_join(df_sites |> mutate(row_id = row_number()), by = "row_id") |>
+  select(-row_id)
+
+# Plot modelled vs observed
+# Vcmax
+df_sites_with_outputs |>
+  ggplot(aes(vcmax_mod, vcmax)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
+  labs(
+    x = "Modelled Vcmax",
+    y = "Observed Vcmax"
+  )
+
+# Jmax
+df_sites_with_outputs |>
+  ggplot(aes(jmax_mod, jmax)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
+  labs(
+    x = "Modelled Jmax",
+    y = "Observed Jmax"
+  )
+
+# Vcmax:Jmax ratio
+df_sites_with_outputs |>
+  mutate(vj_mod = vcmax_mod / jmax_mod) |>
+  ggplot(aes(vj_mod, vj)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
+  labs(
+    x = "Modelled Vcmax:Jmax",
+    y = "Observed Vcmax:Jmax"
+  )
+
+# # Kumarathunge data
+# df <- read_csv("~/data/archive/acitglob_kumarathunge_2020/data/Kumarathunge-aci-tglob-8595d961d4c8/Data/PPC-TGlob_V1.0.csv")
+#
+# # filter data to field - native environment (seed source lon/lat can be interpreted as lon/lat for forcing data)
+# df <- df |>
+#   filter(Growth_condition == "Field (NE)")
+#
+# df_sites <- df |>
+#   ungroup() |>
+#   mutate(vj = Vcmax/Jmax) |>
+#   group_by(Location) |>
+#   summarise(
+#     lon = mean(seed_source_longitude),
+#     lat = mean(seed_source_latitude),
+#     vcmax = mean(Vcmax),
+#     jmax = mean(Jmax),
+#     vj = mean(vj)
+#   )
+#
+# df_sites |>
+#   ggplot(aes(x = vj)) +
+#   geom_histogram(bins = 9)
